922fc59c5101cccb7b5fad9133cbe23818d1182d
Pablo Cruz <cruzrodripablo@gmail.com>
2023-02-09 10:13:28 +0100

    [ADD] chakra icons and search icon

--- src/layout/TopMenu/TopMenu.jsx
+++ src/layout/TopMenu/TopMenu.jsx
@@ -1,11 +1,20 @@
 
 import './TopMenu.css'
 import { Input } from '@chakra-ui/react'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
+import { SearchIcon} from '@chakra-ui/icons'
+
 export default function TopMenu() {
     return (
         <div className='topMenu--whole'>
-            <Input className='inputSearch--bar' width='50vw' placeholder='Search for your game...' />
+            <div>
+                <Input className='inputSearch--bar' width='50vw' placeholder='Search for your game...' />
+                <IconButton className='searchIcon'
+                    colorScheme='blue'
+                    icon={<SearchIcon />}
+                />
+            </div>
             <div>
                 <Button colorScheme='gray'> Settings</Button>
                 <Button colorScheme='twitter'> Klabitos Pablo</Button>
