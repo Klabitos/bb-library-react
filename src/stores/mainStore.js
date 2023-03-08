361de1d4e8603293141bcf4e0eefcc861194b770
Pablo <klabitos@usal.es>
2023-03-08 10:34:07 +0100

    [DEL] delete tests

--- src/stores/mainStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterLogic'
-
-export default configureStore({
-  reducer: {
-    counter: counterReducer //un valor en la Store que va a ser counter y cuya logica esta gestionada por el reducer (counterReducer)
-  }
-})
